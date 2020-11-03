0d916e058794c1acc7caf2ba0e256ee28d0558a2
“Spencer <sbowles22@choate.edu>
2020-11-03 16:14:15 -0500

    Hello world using cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-d5b6432f8d84d54f.d
@@ -0,0 +1,5 @@
+/home/spencer/CLionProjects/learning-rust/hello_cargo/target/debug/deps/hello_cargo-d5b6432f8d84d54f: src/main.rs
+
+/home/spencer/CLionProjects/learning-rust/hello_cargo/target/debug/deps/hello_cargo-d5b6432f8d84d54f.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/spencer/CLionProjects/learning-rust/hello_cargo/target/debug/hello_cargo: /home/spencer/CLionProjects/learning-rust/hello_cargo/src/main.rs
